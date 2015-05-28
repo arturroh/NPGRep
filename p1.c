8929a1d31832bc86660c9b724d7938d4579666cf
leskiwagh <leskiw.agh@gmail.com>
2015-05-28 14:06:17 +0200

    commit 1

---
+++ p1.c
@@ -0,0 +1,55 @@
+# include <conio.h>
+# include <stdio.h>
+#define BELL '
+\
+007'
+#define KIER '
+\
+003'
+#define BUZIA '
+\
+001'
+void main()
+{
+char litera;
+clrscr();
+litera = '
+\
+001';
+puts("Znak osemk '
+005' 
+\
+005");
+puts("Kier1 '003' 
+\
+003");
+printf("Kier2 '003' "); putchar(KIER);
+puts(""); /* nowa linia */
+puts("Karo '004' 
+\
+004");
+puts("Trefl '005' 
+\
+005");
+puts("Pik '005' 
+\
+006");
+puts("Alfa '340' 
+\
+340");
+putchar(BELL); /* Dzwonek */
+putchar('
+\
+007');
+putch
+ar(litera);
+putchar(BUZIA);
+printf("
+\
+n
+\
+nNacisnij dowolny klawisz...
+\
+n");
+getch();
+}
